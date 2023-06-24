8a23f6d0dc57e84a722a4aa1e82ce7189108f88c
jakespirek <jakespirek@gmail.com>
2023-06-24 15:02:56 -0500

    adjustments for site redesign 2023

--- scripts/admission-nav.js
+++ scripts/admission-nav.js
@@ -1,58 +1,55 @@
 // Add the current selector to the on-demand menu item if in these directories
-
-$(document).ready(function() {
-    if (window.location.href.indexOf("process") > -1) {
-		setTimeout(function(){
-			let myFirstLink = document.getElementById('nav-process');
-            myFirstLink.className += (" nav-current");
-		  }, 100);
-	} 
-    if (window.location.href.indexOf("advice") > -1) {
-		setTimeout(function(){
-			let mySecondLink = document.getElementById('nav-advice');
-			mySecondLink.className += (" nav-current");
-		  }, 100);
-	} 	
-    if (window.location.href.indexOf("admissionchat") > -1) {
-		setTimeout(function(){
-			let mySecondLink = document.getElementById('nav-advice');
-			mySecondLink.className += (" nav-current");
-		  }, 100);
-	} 	
-    if (window.location.href.indexOf("tips") > -1) {
-		setTimeout(function(){
-			let mySecondLink = document.getElementById('nav-advice');
-			mySecondLink.className += (" nav-current");
-		  }, 100);
-	} 	
-	if (window.location.href.indexOf("events") > -1) {
-		setTimeout(function(){
-			let myThirdLink = document.getElementById('nav-events');
-            myThirdLink.className += (" w--current");
-		  }, 100);
-    }
-    if (window.location.href.indexOf("services") > -1) {
-		setTimeout(function(){
-			let myFourthLink = document.getElementById('nav-services');
-			myFourthLink.className += (" nav-current");
-		  }, 100);
-    }
-    if (window.location.href.indexOf("help") > -1) {
-		setTimeout(function(){
-			let myFifthLink = document.getElementById('nav-help');
-            myFifthLink.className += (" w--current");
-		  }, 100);
-    }
-    if (window.location.href.indexOf("hc") > -1) {
-		setTimeout(function(){
-			let myFifthLink = document.getElementById('nav-help');
-            myFifthLink.className += (" w--current");
-		  }, 100);
-    }
-
-    else {
-		// nothing to do here, intentionally left blank
+$(document).ready(function () {
+	if (window.location.href.indexOf("process") > -1) {
+	  setTimeout(function () {
+		let myFirstLink = document.getElementById('nav-process');
+		myFirstLink.className += (" w--current");
+	  }, 100);
 	}
-  });
-
+	if (window.location.href.indexOf("advice") > -1) {
+	  setTimeout(function () {
+		let mySecondLink = document.getElementById('nav-advice');
+		mySecondLink.className += (" w--current");
+	  }, 100);
+	}
+	if (window.location.href.indexOf("admissionchat") > -1) {
+	  setTimeout(function () {
+		let mySecondLink = document.getElementById('nav-advice');
+		mySecondLink.className += (" w--current");
+	  }, 100);
+	}
+	if (window.location.href.indexOf("tips") > -1) {
+	  setTimeout(function () {
+		let mySecondLink = document.getElementById('nav-advice');
+		mySecondLink.className += (" w--current");
+	  }, 100);
+	}
+	if (window.location.href.indexOf("events") > -1) {
+	  setTimeout(function () {
+		let myThirdLink = document.getElementById('nav-events');
+		myThirdLink.className += (" w--current");
+	  }, 100);
+	}
+	if (window.location.href.indexOf("services") > -1) {
+	  setTimeout(function () {
+		let myFourthLink = document.getElementById('nav-services');
+		myFourthLink.className += (" w--current");
+	  }, 100);
+	}
+	if (window.location.href.indexOf("help") > -1) {
+	  setTimeout(function () {
+		let myFifthLink = document.getElementById('nav-help');
+		myFifthLink.className += (" w--current");
+	  }, 100);
+	}
+	if (window.location.href.indexOf("hc") > -1) {
+	  setTimeout(function () {
+		let myFifthLink = document.getElementById('nav-help');
+		myFifthLink.className += (" w--current");
+	  }, 100);
+	}
   
+	else {
+	  // nothing to do here, intentionally left blank
+	}
+});
